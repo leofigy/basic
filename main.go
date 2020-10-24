34225ee0b77628ea4c2a08032d5a80d59e10187c
leofigy <angel.figueroa@digitalonus.com>
2020-10-24 12:07:37 -0500

    trying to connect with vault

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
+
+var (
+	// VAULT login server path
+	VAULTSERVER = "http://localhost:8200/v1/auth/contra/login"
+)
+
+func init() {
+	newRoute := os.Getenv("VAULT_AUTH_SERVER")
+	if len(newRoute) > 0 {
+		VAULTSERVER = newRoute
+	}
+}
 
 func main() {
 	router := gin.Default()
@@ -21,10 +38,23 @@ func HelloWeb(c *gin.Context) {
 
 	if c.Request.Method == "POST" {
 
-		user := c.DefaultPostForm("username", "none" )
-		pass := c.DefaultPostForm("password", "none" )
+		user := c.DefaultPostForm("username", "none")
+		pass := c.DefaultPostForm("password", "none")
 
+		token, err := CheckUser(user, pass)
 
+		if err != nil {
+			c.JSON(400, gin.H{
+				"message": err.Error(),
+			})
+		}
+
+		if len(token) == 0 {
+			c.JSON(400, gin.H{
+				"message": "Prohibido !!!!!",
+			})
+			return
+		}
 
 		c.HTML(http.StatusOK, "internal.html", nil)
 		return
@@ -39,8 +69,59 @@ func HelloIndex(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", nil)
 }
 
-func CheckUser(user, pass string){
+func CheckUser(user, pass string) (token string, err error) {
 
-	return 
+	// initialize http client
+	client := &http.Client{}
 
+	data := struct {
+		Password string `json:"password"`
+	}{
+		Password: pass,
+	}
+
+	body, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+
+	userAuth := VAULTSERVER + "/" + user
+
+	// set the HTTP method, url, and request body
+	req, err := http.NewRequest(http.MethodPut, userAuth, bytes.NewBuffer(body))
+	if err != nil {
+		return "", err
+	}
+
+	// set the request header Content-Type for json
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode == 400 {
+		return "", fmt.Errorf("Invalid user")
+	}
+
+	response, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	holder := struct {
+		Auth struct {
+			ClientToken string `json:"client_token"`
+		} `json:"auth"`
+	}{}
+
+	err = json.Unmarshal(response, &holder)
+
+	if err != nil {
+		return "", err
+	}
+
+	return holder.Auth.ClientToken, nil
 }
